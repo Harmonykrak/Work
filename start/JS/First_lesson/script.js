be4dd6ad28fdf94a44af1d29513fa60b49b94bd8
Maks <maks0978027942@gmail.com>
2022-10-06 12:52:17 +0300

    add function

--- start/JS/First_lesson/script.js
+++ start/JS/First_lesson/script.js
@@ -1,10 +1,35 @@
-'use strict'
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+'use strict';
+
+
+
 
 // const result = confirm('Are you Okey?');
 // console.log(result);
 
-const numberOfFilms = +prompt("How much films did you look?", "");
-console.log(numberOfFilms);
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt("How much films did you look?", "");
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How much films did you look?", "");
+
+    }
+}
+
+start();
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -14,16 +39,46 @@ let personalMovieDB = {
     privat: false
 };
 
-for(let i=1; i<=numberOfFilms; i++){
-    const namefilm = prompt("Last film wich you looked?", "");
-    const ratingfilm = prompt("How rating was this film?", "");
-    if(namefilm.length < 50  && namefilm != null && ratingfilm != null && namefilm != "" && ratingfilm != "") {
-        personalMovieDB.movies[namefilm] = ratingfilm;
-        console.log('done');
-    }else {
-        alert("Please, can you write last film wich you looked?");
-        i--;    
+function RememberMyFilm(){
+    for(let i=1; i<=numberOfFilms; i++){
+        const namefilm = prompt("Last film wich you looked?", "");
+        const ratingfilm = prompt("How rating was this film?", "");
+        if(namefilm.length < 50  && namefilm != null && ratingfilm != null && namefilm != "" && ratingfilm != "") {
+            personalMovieDB.movies[namefilm] = ratingfilm;
+            console.log('done');
+        }else {
+            alert("Please, can you write last film wich you looked?");
+            i--;    
+        }
     }
 }
-personalMovieDB.count < 10 ?  alert("you looked little films") :  alert("you looked a lot of films");
-console.log(personalMovieDB);
+
+RememberMyFilm();
+
+function MyPersenalLevel(){
+    personalMovieDB.count < 10 ?  alert("you looked little films") :  alert("you looked a lot of films");
+}
+
+MyPersenalLevel();
+
+function writeYourGenres(){
+    for(let i=1; i<=numberOfFilms; i++){
+        const namegenres = prompt(`Your lovely genere number ${i} is?`, "");
+        const numbergeners = i-1;
+        if(namegenres.length < 50  && namegenres != null &&  namegenres != "" ) {
+            personalMovieDB.genres[numbergeners] = namegenres;
+            console.log('done');
+        }else {
+            alert("Please, can you write last film wich you looked?");
+            i--;    
+        }
+    }
+}
+
+writeYourGenres();
+
+function showMyDB(){
+    personalMovieDB.privat == false ? console.log(personalMovieDB) : alert("DataBase Privat, Sorry");
+}
+
+showMyDB();
