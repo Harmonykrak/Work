1466f9ddc7e3bad906c10b1b121289577ae91cea
Maks <maks0978027942@gmail.com>
2022-10-17 15:01:27 +0300

    add new functional

--- start/interact with file by JS/030 project-mDB/project/js/script.js
+++ start/interact with file by JS/030 project-mDB/project/js/script.js
@@ -1,47 +1,100 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
-'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против...",
-        "Абракадабра"
-    ]
-};
+document.addEventListener("DOMContentLoaded", () => {
+    'use strict';
 
-const adv = document.querySelector(".promo__adv"),
-      poster = document.querySelector(".promo__bg"),
-      genre = poster.querySelector(".promo__genre"),
-      movieList = document.querySelector(".promo__interactive-list");
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против...",
+            "Абракадабра"
+        ]
+    };
 
-adv.remove();
-genre.textContent = "Drama";
-poster.style.backgroundImage = 'url("img/bg.jpg")';
-movieList.innerHTML = "";
+    const adv = document.querySelector(".promo__adv"),
+        poster = document.querySelector(".promo__bg"),
+        genre = poster.querySelector(".promo__genre"),
+        movieList = document.querySelector(".promo__interactive-list"),
+        addForm = document.querySelector(".add"),
+        addInput = addForm.querySelector(".adding__input"),
+        checkbox = addForm.querySelector("[type='checkbox']");
 
-movieDB.movies.sort();
+    addForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i+1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});
- 
+        let NewFilm = addInput.value;
+        const favorite = checkbox.cheked;
+
+        if(NewFilm) {
+
+            if(NewFilm.lenght > 21){
+                NewFilm = `${NewFilm.substring(0, 22)}...`;
+            }
+
+            movieDB.movies.push(NewFilm);
+
+            newList(movieDB.movies, movieList);            
+        }
+
+        e.target.reset();
+    });
+
+
+    const delateAdv = (arr) => {
+        arr.remove();
+    };
+
+    delateAdv(adv);
+
+    genre.textContent = "Drama";
+    poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    function newList(films, parent) {
+        parent.innerHTML = "";
+
+        sortArr(movieDB.movies);
+
+        films.forEach((film, i) => {
+            movieList.innerHTML += `
+                <li class="promo__interactive-item">${i+1} ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                films.splice(i, 1);
+
+                newList(films, parent);
+            });
+        });
+    }
+    
+    newList(movieDB.movies, movieList);
+
+}); 
